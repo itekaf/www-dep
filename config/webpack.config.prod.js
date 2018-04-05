adfceb22cd0b950940398083bb874cd2d727a1c0
xferra <xferra@live.com>
2018-04-05 09:38:47 +0300

    Move git module to ext folder

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -8,6 +8,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
@@ -329,6 +330,11 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.components:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    // Copy files plugin
+    new CopyWebpackPlugin([{ 
+      from: 'ext/assets/raster/lh-logo-border.png',
+      to: 'assets/raster/lh-logo-border.png'
+    }]),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
