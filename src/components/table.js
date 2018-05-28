e995a9b798cd38d4bb7b08c259f80c2053cf4c10
svoboda-rabstvo <romanrostislavovich@gmail.com>
2018-05-28 21:26:46 +0300

    Changes on remarks

--- src/components/table.js
+++ src/components/table.js
@@ -7,10 +7,23 @@ class LintersTable extends Component {
     super(props);
     this.state = {
       sortType: 'name',
-      sortDirection: 'desc',
+      sortDirection: 'desc', 
+      value: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
     this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.setFilter(e.target.value);
+    this.setState({value: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.setFilter(this.state.value);
   }
 
   handleClick(e) {
@@ -23,19 +36,36 @@ class LintersTable extends Component {
   }
 
   render() {
-    const columns =  ['name', 'description', 'languages', 'license'];
-  
+    const columns = [
+      {
+        "name": "name",
+        "class": "th-small cell100"
+      },
+      {
+        "name": "description",
+        "class": "th-big cell100",
+      },
+      {
+        "name": "languages",
+        "class": "th-big hidden-xm cell100",
+      },
+      {
+        "name": "license",
+        "class": "th-small hidden-lg cell100",
+      }
+    ];
+
     const header = columns.map((item, index) => {
       return (
-        <th key={index} className={item === "description" ? "hidden-xs cell100" : "cell100"}>
-          <a href="#!" name={item} onClick={this.handleClick}>
-            {item[0].toUpperCase() +  item.substr(1)}
-            {sortSpan(this.state, item)}
+        <th key={index} className={item.class}>
+            <a href="#!" name={item.name} onClick={this.handleClick}>
+            {item.name}
+            {sortSpan(this.state, item.name)}
           </a>
         </th>
       );
-    });
-
+    })
+  
     const filteredLinters = _.filter(this.props.data, (linter) => {
 
         var searchData = this.props.search;
@@ -54,30 +84,44 @@ class LintersTable extends Component {
       return (
         <tr key={index} className="table-linterhub">
           <td className="cell100"><a href={item.url}>{item.name}</a></td>
-          <td className="hidden-xs cell100">{item.description}</td>
-          <td className="cell100">{tagLanguage(item.languages)}</td>
-          <td className="cell100">{item.license}</td>
+          <td className="cell100">{item.description}</td>
+          <td className="cell100 hidden-xm">{tagLanguage(item.languages)}</td>
+          <td className="cell100 hidden-lg">{item.license}</td>
         </tr>
       );
     });
 
-    return (
-      <div>
-        {/* <h4>Showing {filteredLinters.length} Linter
-        <span className={filteredLinters.length === 1 ? 'hidden' : ''}>s</span>.</h4> */}
-        <div className="table100 ver3 m-b-110">
-        <table className="table table-bordered table-striped">
-          <thead className="table100-head"><tr>{header}</tr></thead>
-          <tbody className="table100-body">{content}</tbody>
-        </table>
+    const search = <div className="search">
+      <form onSubmit={this.handleSubmit} >
+        <div className="form-group">
+          <div className="input-group container-flex">
+            <div className="input-group-addon">
+              <i className="fontello-icon">&#xe800;</i>
+            </div>
+            <input type="text"
+                    className="form-control"
+                    placeholder="Search by me"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+            />
+          </div>
         </div>
-        
+      </form>
+    </div>
+
+    return (
+      <div className="tab-content">
+        {search}
+        <div className="table100 ver3 m-b-110">
+          <table className="table table-bordered table-striped">
+            <thead className="table100-head"><tr>{header}</tr></thead>
+            <tbody className="table100-body">{content}</tbody>
+          </table>
+        </div>
       </div>
     );
   }
 }
-
-export default LintersTable;
 
 function sortSpan(e, item) {
   if (item === e.sortType){
@@ -101,3 +145,6 @@ function tagLanguage(data) {
   });
   return result;
 }
+
+export default LintersTable;
+
